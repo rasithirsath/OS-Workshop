ccb0d7ced6471097ca29e1f4d51b62e6d8c956e6
full-StackDevelopers <rasithshahul@gmail.com>
2025-08-24 00:33:15 +0530

    Files Added

---
+++ HTML-STARTER/content.md
@@ -0,0 +1,9 @@
+## ABOUT
+
+I am an experienced Full-stack Developer and UI/UX Designer with Five years plus experience building and designing websites and web apps from scratch. HTML, CSS, SCSS, Javascript, Angular, Nodejs, Reactjs, Mongo DB, and Framework are the field of my expertise when it comes to building websites and web apps. I deal with servers as well. I am flawless using Figma, Adobe XD, and Photoshop for the design. Photography is also one of the skills I use in my design.
+
+## COLOR PALETTE
+
+BUTTON COLOR = #e67e22
+H1 TEXT COLOR = #333333
+PARAGRAPH TEXT COLOR =#555555
---
+++ JS-STARTER/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//////////////////
+//////// DOM MANIPULATION
+/*
+const btn = document.querySelector(".changeBgBtn");
+console.log(btn);
+
+const heading = document.querySelector(".heading");
+console.log(heading);
+
+btn.addEventListener("click", function () {
+  // Generate random RGB values
+  const r = Math.floor(Math.random() * 226);
+  const g = Math.floor(Math.random() * 226);
+  const b = Math.floor(Math.random() * 226);
+
+  // Create rgb string
+  const rgbColor = `rgb(${r}, ${g}, ${b})`;
+
+  // Set new background
+  document.body.style.background = rgbColor;
+
+  // Update heading text with the RGB value
+  heading.textContent = rgbColor;
+});
+*/
+
+//////////////////
+//////// JAVASCRIPT FUNDAMENTALS
