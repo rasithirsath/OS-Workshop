a1a1454fa32f1c29c1a8e6bc7efc2a6d46fb5486
full-StackDevelopers <rasithshahul@gmail.com>
2025-08-24 19:50:38 +0530

    Changes Made

--- GAME-STARTER/script.js
+++ GAME-STARTER/script.js
@@ -1,10 +1,8 @@
 'use strict';
 // Variables needed for Game login
-
-// Working in check button
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-/////////////////////
-////// GAME LOGIC'S
+////////////////
+///// GAME LOGIC
